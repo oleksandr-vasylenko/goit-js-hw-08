91d4255a6acc6028029eb4930466933efa498bbe
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-09-29 10:49:55 +0300

    Completes 1st part of Task #3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('submit', onSubmit);
+refs.textarea.addEventListener('input', throttle(onInput, 500));
+
+fillMessage();
+
+function onSubmit(e) {
+  e.preventDefault();
+  console.log('Form has been submitted!');
+  e.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function onInput(e) {
+  const message = e.target.value;
+  localStorage.setItem('feedback-form-state', message);
+}
+
+function fillMessage() {
+  const savedMessage = localStorage.getItem('feedback-form-state');
+
+  if (savedMessage) {
+    refs.textarea.value = savedMessage;
+  }
+}
