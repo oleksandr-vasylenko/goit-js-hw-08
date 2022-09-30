3eb67dea9b1fe5c9cb7f56e23f96fcd24cefd51f
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-09-30 13:55:33 +0300

    Completes Task #02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,7 +3,17 @@ import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
+let timeData = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+
+openPage();
+
+function openPage() {
+  if (timeData) {
+    player.setCurrentTime(timeData);
+  }
+}
+
 player.on('timeupdate', function (data) {
-  let timeData = Math.round(data.seconds);
+  timeData = Math.round(data.seconds);
   localStorage.setItem('videoplayer-current-time', timeData);
 });
